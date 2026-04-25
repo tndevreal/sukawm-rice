c546168b34c41da4fdc7aef57d692694d6a5d764
tndev <tndevreal@protonmail.com>
2026-04-25 20:34:17 +0000

    Refactor install.sh for improved clarity and efficiency

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e
 
 USER_NAME="$(whoami)"
@@ -17,23 +16,20 @@ case "$confirm" in
     *) echo "Invalid input. Exiting."; exit 1 ;;
 esac
 
-if   command -v xbps-install >/dev/null 2>&1; then
+if command -v xbps-install >/dev/null 2>&1; then
     DISTRO="void"
 elif command -v pacman >/dev/null 2>&1 && ! command -v artix-help >/dev/null 2>&1; then
     DISTRO="arch"
 elif command -v pacman >/dev/null 2>&1 && command -v artix-help >/dev/null 2>&1; then
     DISTRO="artix"
 else
-    echo "Unsupported distro. Only Void, Arch, and Artix are supported."
+    echo "Unsupported distro."
     exit 1
 fi
 
-DOAS_CONF=$(cat <<EOF
-permit :wheel
+DOAS_CONF="permit :wheel
 permit nopass $USER_NAME
-permit nopass root
-EOF
-)
+permit nopass root"
 
 echo "$DOAS_CONF" | doas tee /etc/doas.conf >/dev/null
 doas chmod 600 /etc/doas.conf
@@ -45,44 +41,35 @@ case "$DISTRO" in
         doas xbps-install -Sy dunst rofi polybar picom flatpak feh \
             xdg-desktop-portal-gtk kitty polkit-gnome doas
         ;;
-    arch)
-        doas pacman -Sy --noconfirm dunst rofi polybar picom flatpak feh \
-            xdg-desktop-portal-gtk kitty polkit-gnome opendoas
-        ;;
-    artix)
+    arch|artix)
         doas pacman -Sy --noconfirm dunst rofi polybar picom flatpak feh \
             xdg-desktop-portal-gtk kitty polkit-gnome opendoas
         ;;
 esac
 
-doas chown $USER : $USER * 
-
-echo "Configuring doas..."
 echo "Removing old configs..."
-
-rm -rf "$HOME_DIR/.config/dunst"
-rm -rf "$HOME_DIR/.config/rofi"
-rm -rf "$HOME_DIR/.config/polybar"
-rm -rf "$HOME_DIR/.config/suka"
-rm -rf "$HOME_DIR/.config/picom"
+rm -rf "$HOME_DIR/.config/dunst" \
+       "$HOME_DIR/.config/rofi" \
+       "$HOME_DIR/.config/polybar" \
+       "$HOME_DIR/.config/suka" \
+       "$HOME_DIR/.config/picom"
 
 echo "Copying new configs..."
-
 mkdir -p "$HOME_DIR/.config"
 
-doas -r cp -r picom "$HOME_DIR/.config/"
-doas -r cp -r rofi "$HOME_DIR/.config/"
-doas -r cp -r suka "$HOME_DIR/.config/"
-doas -r cp -r polybar "$HOME_DIR/.config/"
-doas -r cp -r dunst "$HOME_DIR/.config/"
+cp -r picom "$HOME_DIR/.config/"
+cp -r rofi "$HOME_DIR/.config/"
+cp -r suka "$HOME_DIR/.config/"
+cp -r polybar "$HOME_DIR/.config/"
+cp -r dunst "$HOME_DIR/.config/"
 
 mkdir -p "$HOME_DIR/Downloads"
 cp walp.png "$HOME_DIR/Downloads/walp.png"
-echo "default bindings are:"
-echo "super+a=rofi"
-echo "super+LMB=move window/focus window"
-echo "super+RMB=resize window"
-echo "super+control+LMB= move full canvas"
-echo "super+t(or enter/return)=kitty/terminal"
-echo "done."
 
+echo "default bindings are:"
+echo "super+a = rofi"
+echo "super+LMB = move window/focus window"
+echo "super+RMB = resize window"
+echo "super+control+LMB = move full canvas"
+echo "super+t (or enter) = kitty terminal"
+echo "done."
